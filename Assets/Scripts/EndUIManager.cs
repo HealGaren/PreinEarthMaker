2a285657d4cfa2623016296cd462d9c260442926
HealGaren <qkswkd7003@naver.com>
2017-07-25 04:36:43 +0900

    엔딩 추가

---
+++ Assets/Scripts/EndUIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndUIManager : MonoBehaviour {
+
+    public Animator fadeInOutImage;
+    public bool isToStartScene = false;
+	// Use this for initialization
+	void Start () {
+        fadeInOutImage.SetBool("isShow", false);
+        StartCoroutine(DelayNextScene());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator DelayNextScene()
+    {
+        yield return new WaitForSeconds(3f);
+        fadeInOutImage.SetBool("isShow", true);
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(isToStartScene ? "Title" : "InGame");
+    }
+}
